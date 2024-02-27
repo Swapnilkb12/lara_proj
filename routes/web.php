2bc17dcd4a0ec8a4addd95d61c99fdbf2ae611f3
swapnilkb12 <swapnil12kb@gmail.com>
2024-02-27 23:29:12 +0530

    revising the concepts

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,27 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function(){
+    return view("welcome");
+});
 
-Route::get('/', function () {
-    return view('welcome');
+//creating the route
+
+Route::get('/teet', function () {
+    return view('home');
+});
+//creating the route for displaying the data
+// this data canbe assecible in blade file also
+Route::get('/teet/{name}/{data?}', function ($name,$data=null) {
+    // dd($data);
+    $sum=compact('name','data');
+    // print_r($sum);
+    return view('demo')->with($sum);
+   
+});
+
+//passing the data 
+
+Route::get('/name/{data}', function($data){
+    return view('demo');
 });
